c4ffacf5cbd43e88f516a544115cad17652a0a3e
Littlei0409 <okokaran@gmail.com>
2025-10-03 10:05:31 +0800

    w03

---
+++ w03_blogs_86/src/pages/HomeLayoutPage_86.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
+const HomeLayoutPage_86 = () => {
+  return (
+  <>
+    {/* <Navbar /> */}
+    <Outlet />
+  </>
+  ) 
+}
+export default HomeLayoutPage_86;
---
+++ w03_blogs_86/src/pages/index.jsx
@@ -0,0 +1,4 @@
+export {default as HomeLayoutPage_86} from './HomeLayoutPage_86.jsx';
+export {default as HomePage_86} from './HomePage_86.jsx';
+export {default as BlogStaticPage_86} from './BlogStaticPage_86.jsx';
+export {default as BlogLocalJsonPage_86} from './BlogLocalJsonPage_86.jsx';
