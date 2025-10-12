0f02adde561152902f12f5882511f7be1563bd12
Littlei0409 <okokaran@gmail.com>
2025-10-12 23:39:14 +0800

    commit

---
+++ w04_blogs_fullstack_86/client_blogs_86/src/pages/BlogLocalJsonPage_86.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+// import blogData_86 from '../assets/data/blogData.json';
+import Blog_86 from '../components/Blog_86';
+
+const BlogLocalJsonPage_86 = () => {
+  const [name] = useState('kunsiang');
+  const [id] = useState(213410086);
+  const [blogs_86, setBlogs_86] = useState(blogData_86);
+
+  return (
+    <section className="blogs">
+      <div className="section-title">
+        <h2>blogs from local json -- {name}, {id}</h2>
+      </div>
+
+      <div className="blogs-center">
+        {blogs_86.map((item) => {
+          const { id: blogId, title, descrip, category, img } = item;
+          return (
+            <Blog_86
+              key={blogId ?? title}
+              id={blogId}
+              title={title}
+              descrip={descrip}
+              category={category}
+              img={img}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default BlogLocalJsonPage_86;
---
+++ w04_blogs_fullstack_86/client_blogs_86/src/pages/HomePage_86.jsx
@@ -0,0 +1,9 @@
+const HomePage_86 = () => {
+  return (
+    <div>
+      <h1>HomePage_86</h1>
+      <h2>kunsiang, 213410086</h2>
+    </div>
+  )
+}
+export default HomePage_86
